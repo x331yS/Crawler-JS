03468091eedebd3a044d0bd4fa357b7072240c87
FLEJOSNE <florian.lejosne@ynov.com>
2021-05-10 16:35:07 +0200

    [ADD] file

--- movies.js
+++ movies.js
@@ -1,13 +1,12 @@
-const puppeteer = require("puppeteer");
-const {delay} = require('./amazon')
 const fs = require('fs')
-const path = require("path")
+const path = require('path')
+const {promisify} = require('util')
+const puppeteer = require('puppeteer');
+const writeFileAsync = promisify(fs.writeFile);
+const {delay} = require('./amazon')
 const https = require("https");
 
 async function imdb(str) {
-    metaScore(str)
-    price(str)
-    allocine(str)
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.setViewport({width: 1900, height: 1000})
@@ -24,7 +23,6 @@ async function imdb(str) {
     const time = await page.evaluate(() => {
         return document.querySelector("div[class='subtext'] > time").innerText;
     })
-
     let top = await page.evaluate(() => {
         let pls = document.querySelector("div[id='titleAwardsRanks']") !== null;
         if (pls) {
@@ -33,24 +31,14 @@ async function imdb(str) {
             top = "No top ranking"
         }
     })
-
-
     const ratingNumber = await page.evaluate(() => {
         return document.querySelector('span[itemprop="ratingCount"]').innerText;
     })
-    console.log("Titre :", title)
-    console.log('Temps :', time)
-    console.log("Note", ratingValue)
-    console.log("Rank", top)
-    console.log("Nb de vote ", ratingNumber)
-
-
     await page.click('img')
     await delay(3000)
     const url = await page.evaluate(() => {
         return document.querySelector(".media-viewer img").src;
     })
-    console.log(url)
     https.get(url, (data) => {
         const extension = path.extname(url);
         const stream = fs.createWriteStream(`./static/movies/movie${extension}`);
@@ -60,6 +48,169 @@ async function imdb(str) {
         });
     });
     await browser.close()
+    console.log("imdb")
+    let metaScore = " No metaScore"
+    let nbCritique = " No critique"
+    if (str !== "harry potter") {
+        const newBrowser = await puppeteer.launch()
+        const metascorepage = await newBrowser.newPage()
+        await delay(3000)
+        str = str.replace(" ", "%20")
+        await metascorepage.goto(`https://www.metacritic.com/search/all/${str}/results`)
+        await delay(6000)
+        await metascorepage.evaluate(() => {
+            let pls = document.querySelector("#onetrust-accept-btn-handler") !== null;
+            if (pls == null) {
+                delay(15000)
+            }
+        })
+        await metascorepage.click("#onetrust-accept-btn-handler")
+        await delay(4000)
+        const metaScore = await metascorepage.evaluate(() => {
+            return document.querySelector(".main_stats > span").innerText;
+        })
+        await metascorepage.click("h3 > a")// .product_title
+        await metascorepage.waitForSelector('.distribution');
+        let nbCritique = await metascorepage.evaluate(() => {
+            return document.querySelector(".based_on").innerText;
+        })
+        await delay(2000)
+        const element = await metascorepage.$('#nav_to_metascore');
+        await element.screenshot({path: 'static/movies/metascoreditrib.png'})
+
+        await newBrowser.close()
+    }
+    console.log("metascore")
+    const price = await puppeteer.launch({headless:false});
+    const pricepage = await price.newPage()
+    await pricepage.goto(`https://www.amazon.com/s?k=${str}+film`)
+    await delay(10000)
+    await pricepage.evaluate(() => {
+        let pls = document.querySelector(".a-button-input") !== null;
+        if (pls == null) {
+            delay(15000)
+        }
+    })
+    await pricepage.click('.a-button-input')
+    const prix = await pricepage.evaluate(() => {
+        return document.querySelector(".a-offscreen").innerText;
+    })
+    await price.close();
+    console.log("amazon")
+    const allo = await puppeteer.launch();
+    const allopage = await allo.newPage()
+    await allopage.goto(`https://www.allocine.fr/rechercher/?q=${str}`)
+    await delay(1000)
+    await allopage.click('.jad_cmp_paywall_button-cookies')
+    const date = await allopage.evaluate(() => {
+        return document.querySelector(".date").innerText;
+    })
+    const director = await allopage.evaluate(() => {
+        return document.querySelector(".meta-body-direction").innerText;
+    })
+    const actor = await allopage.evaluate(() => {
+        return document.querySelector(".meta-body-actor").innerText;
+    })
+    await allopage.click('.meta-title-link')
+    await allopage.waitForSelector(".content-txt")
+    const synopsis = await allopage.evaluate(() => {
+        return document.querySelector(".content-txt").innerText;
+    })
+    await allo.close()
+    console.log("allocine")
+    const yobrowser = await puppeteer.launch();
+    const youpage = await yobrowser.newPage()
+    await youpage.goto(`https://www.youtube.com/results?search_query=${str}+trailer`)
+    await delay(1000)
+    await youpage.click("button >.VfPpkd-Jh9lGc")
+    await delay(3000)
+    await youpage.click(".yt-img-shadow")
+    const youtubeUrl = await youpage.url();
+    let youtubehurle = youtubeUrl.replace("watch?v=", "embed/");
+    await yobrowser.close()
+    console.log("youtube")
+    await writeFileAsync(path.join(`static/movies/page.html`), "<!DOCTYPE html>\n" +
+        "<html lang=\"en\">\n" +
+        "<head>\n" +
+        "    <meta charset=\"UTF-8\">\n" +
+        "</head>\n" +
+        "<style>\n" +
+        "    * {\n" +
+        "        margin: 0px;\n" +
+        "        font-family: 'Poppins', sans-serif;\n" +
+        "    }\n" +
+        "\n" +
+        "    body {\n" +
+        "        text-align: center;\n" +
+        "        background: #000;\n" +
+        "        padding: 4%;\n" +
+        "    }\n" +
+        "    h3 {\n" +
+        "        color: #fff;\n" +
+        "    }\n" +
+        "    h1 {\n" +
+        "        font-size: 20px;\n" +
+        "        color: #fff;\n" +
+        "        padding: 20px;\n" +
+        "    }\n" +
+        "    h1:hover {\n" +
+        "        color: red;\n" +
+        "    }\n" +
+        "\n" +
+        "    h2 {\n" +
+        "        color: #fff;\n" +
+        "        padding: 10px;\n" +
+        "    }\n" +
+        "    img {\n" +
+        "        position: relative;\n" +
+        "    }\n" +
+        "    iframe {\n" +
+        "        position: relative;\n" +
+        "    }\n" +
+        "    .object {\n" +
+        "        left: 400px;\n" +
+        "        top: 800px;\n" +
+        "\n" +
+        "\n" +
+        "            }\n" +
+        "\n" +
+        "</style>\n" +
+        "<body>\n" +
+        "\n" +
+        "<div>\n" +
+        "    <h2>" + title + "</br>Time of the movie : " + time + "</h2>\n" +
+        "    </br>\n"+
+        "    <h3>" + synopsis + "</h3>\n" +
+        "    </br>\n"+
+        "    <h3>This movie was released in"+ date +"</h3>\n" +
+        "    <img src=\"movie.jpg\" height=\"800\" width=\"561\">\n" +
+        "    <iframe width=\"1050\" height=\"800\" src='" + youtubehurle + "' frameborder=\"0\"\n" +
+        "            allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\"\n" +
+        "            allowfullscreen></iframe>\n" +
+        "    <a href=\"movie.jpg\">\n" +
+        "        <h2>Screen shot link</h2></a>\n" +
+        "    <h3>Production : "+ director+"</h3>\n" +
+        "    <h3>Actor : "+ actor+"</h3>\n" +
+        "    </br>\n"+
+        "    <h3>Amazon price : "+ prix+"</h3>\n" +
+        "    </br>\n"+
+        "    <h2>Score</h2>\n"+
+        "    </br>\n"+
+        "    <h3>Reward : "+ top +" with "+ ratingNumber+"</h3>\n" +
+        "    </br>\n"+
+        "    <h3>Imdb Score on imdb.com : "+ ratingValue +"/10</h3>\n" +
+        "    </br>\n"+
+        "    <h3>MetaScore on metacritique.com : "+ metaScore +"/100 "+ nbCritique+"</h3>\n" +
+        "    </br>\n"+
+        "    <h3>Critique exemple on metacritique.com :</h3>\n" +
+        "    </br>\n"+
+        "    <img src=\"metascoreditrib.png\" height=\"2105\" width=\"620\">\n" +
+
+        "</div>\n" +
+        "\n" +
+        "\n" +
+        "</body>\n" +
+        "</html>")
 }
 
 async function metaScore(str) {
@@ -67,7 +218,7 @@ async function metaScore(str) {
     const metascorepage = await newBrowser.newPage()
     await delay(3000)
     await metascorepage.goto(`https://www.metacritic.com/search/all/${str}/results`)
-    await delay(10000)
+    await delay(6000)
     await metascorepage.click("#onetrust-accept-btn-handler")
     await delay(4000)
     const metaScore = await metascorepage.evaluate(() => {
@@ -89,11 +240,11 @@ async function metaScore(str) {
 
 async function price(str) {
     const price = await puppeteer.launch();
-    const page = await price.newPage()
-    await page.goto(`https://www.amazon.com/s?k=${str}+film`)
-    await delay(1000)
-    await page.click('.a-button-input')
-    const prix = await page.evaluate(() => {
+    const pricepage = await price.newPage()
+    await pricepage.goto(`https://www.amazon.com/s?k=${str}+film`)
+    await delay(4000)
+    await pricepage.click('.a-button-input')
+    const prix = await pricepage.evaluate(() => {
         return document.querySelector(".a-offscreen").innerText;
     })
     console.log(prix)
@@ -102,22 +253,22 @@ async function price(str) {
 
 async function allocine(str) {
     const allo = await puppeteer.launch();
-    const page = await allo.newPage()
-    await page.goto(`https://www.allocine.fr/rechercher/?q=${str}`)
+    const allopage = await allo.newPage()
+    await allopage.goto(`https://www.allocine.fr/rechercher/?q=${str}`)
     await delay(1000)
-    await page.click('.jad_cmp_paywall_button-cookies')
-    const date = await page.evaluate(() => {
+    await allopage.click('.jad_cmp_paywall_button-cookies')
+    const date = await allopage.evaluate(() => {
         return document.querySelector(".date").innerText;
     })
-    const director = await page.evaluate(() => {
+    const director = await allopage.evaluate(() => {
         return document.querySelector(".meta-body-direction").innerText;
     })
-    const actor = await page.evaluate(() => {
+    const actor = await allopage.evaluate(() => {
         return document.querySelector(".meta-body-actor").innerText;
     })
-    await page.click('.meta-title-link')
-    await page.waitForSelector(".content-txt")
-    const synopsis = await page.evaluate(() => {
+    await allopage.click('.meta-title-link')
+    await allopage.waitForSelector(".content-txt")
+    const synopsis = await allopage.evaluate(() => {
         return document.querySelector(".content-txt").innerText;
     })
     console.log(director)
@@ -127,5 +278,17 @@ async function allocine(str) {
     await allo.close()
 }
 
-module.exports = {imdb}
+async function youtube(str) {
+    const yobrowser = await puppeteer.launch();
+    const youpage = await yobrowser.newPage()
+    await youpage.goto(`https://www.youtube.com/results?search_query=${str}+trailer`)
+    await delay(1000)
+    await youpage.click("button >.VfPpkd-Jh9lGc")
+    await delay(3000)
+    await youpage.click(".yt-img-shadow")
+    const youtubeUrl = await youpage.url();
+    console.log(youtubeUrl);
+    await yobrowser.close()
+}
 
+module.exports = {imdb}
